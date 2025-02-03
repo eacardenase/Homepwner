65da0c5aa7a47abc159050370494de3e02251f05
Edwin Cardenas <ecardenas@lwolf.com>
2025-02-03 13:53:26 -0500

    feat: add bnritem model

---
+++ Homepwner/BNRItem.h
@@ -0,0 +1,31 @@
+//
+//  BNRItem.h
+//  Homepwner
+//
+//  Created by Edwin Cardenas on 3/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BNRItem : NSObject
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
+// Convenience method
++ (instancetype)randomItem;
+
+// Designated initializer for BNRItem
+- (instancetype)initWithItemName:(NSString *)name
+                  valueInDollars:(int)value
+                    serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Homepwner/BNRItem.m
@@ -0,0 +1,74 @@
+//
+//  BNRItem.m
+//  Homepwner
+//
+//  Created by Edwin Cardenas on 3/02/25.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+
++ (instancetype)randomItem
+{
+    NSArray *randomAdjectivesList = @[@"Fluffy", @"Rusty", @"Shiny"];
+    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectivesList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    
+    NSString *randomName = [NSString stringWithFormat:@"%@ %@",
+                            randomAdjectivesList[adjectiveIndex], randomNounList[nounIndex]];
+    
+    int randomValue = arc4random() % 100;
+    
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10];
+    
+    BNRItem *newItem = [[self alloc] initWithItemName:randomName
+                                       valueInDollars:randomValue
+                                         serialNumber:randomSerialNumber];
+    
+    return newItem;
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"%@ (%@): Worth $%d, recorded on %@", self.itemName, self.serialNumber, self.valueInDollars, self.dateCreated];
+}
+
+- (instancetype)initWithItemName:(NSString *)name valueInDollars:(int)value serialNumber:(NSString *)sNumber
+{
+    if (self = [super init]) {
+        _itemName = name;
+        _valueInDollars = value;
+        _serialNumber = sNumber;
+        _dateCreated = [NSDate date];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithItemName:(NSString *)name
+{
+    return [self initWithItemName:name
+                   valueInDollars:0
+                     serialNumber:@""];
+}
+
+- (instancetype)init
+{
+    return [self initWithItemName:@"Item"];
+}
+
+- (void)dealloc
+{
+    NSLog(@"Destroyed: %@", self);
+}
+
+
+@end
