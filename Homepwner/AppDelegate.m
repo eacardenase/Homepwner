2f2891d3e49ef6771d94c731fcb4f8edbfb38d2c
Edwin Cardenas <ecardenas@lwolf.com>
2025-02-24 21:36:48 -0500

    feat: review application state transitions

--- Homepwner/AppDelegate.m
+++ Homepwner/AppDelegate.m
@@ -16,6 +16,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
     return YES;
 }
 
@@ -34,6 +37,11 @@
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 
